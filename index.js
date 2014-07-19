fd36c382c1d3ebca4eb8a3da5b07630a36bc369a
Tyler Stark <theirondeveloper@gmail.com>
2014-07-19 10:26:59 -0700

    Updated the code, added a package.json

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
-/** Add a new function to the console object
+/**
+ * Console.Star mimics the behaviour of console.log, but also wraps the log in stars
  *
+ * Usage:
+ * console.star('Hello %s', 'World');
+ *
+ * Result:
+ * ***************
+ * ***************
+ * Hello World
+ * ***************
+ * ***************
+ *
+ * @type {{starString: string, lines: number, logType: string}}
  */
-console.star = function(toConsole, options) {
-	var log = this.log;
-	log.call(this, '********');
-        log.call(this, '********');
-	log.call(this, toConsole);
-        log.call(this, '********');
-        log.call(this, '********');
+
+
+/**
+ * Default Options, can be overridden at any time
+ * @type {{starString: string, lines: number, logType: string}}
+ */
+console.starOptions = {
+	'starString': '***************',
+	'lines': 2,
+	'logType': 'log'
+};
+
+/**
+ * The console[log] wrapper, which pre/post-pends stars
+ */
+console.star = function() {
+	var options = this.starOptions,
+		log = this[options.logType],
+		lines = options.lines,
+		starString = options.starString;
+
+	// Pre-log Stars
+	while(lines--){log.call(this, starString);}
+
+	// Apply the original console[log]
+	log.apply(this, arguments);
+
+	// Reset the count, post-log stars
+	lines = options.lines
+	while(lines--){log.call(this, starString);}
 }
 
-console.star('Test');
+exports = console.star;
